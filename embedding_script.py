291ffe9ec6a1e091fed12f9794a3bf6bea445c1e
Jha <nilpa.jha@accenture.com>
2017-08-01 06:39:37 +0530

    adding embedding script

---
+++ embedding_script.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import collections
+import math
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import pandas as pd
+
+
+
+
+  
+dat = pd.read_csv("sample_dat.csv") 
+text_dat = map(lambda x:  clean_str(x), dat)
+words = sum()
+
+print 'Data size', len(words)
+
+vocabulary_size = 20000 # Based on the words count
+
+def build_dataset(words):
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+  dictionary = dict()
+  for word, _ in count:
+    dictionary[word] = len(dictionary)
+  data = list()
+  unk_count = 0
+  for word in words:
+    if word in dictionary:
+      index = dictionary[word]
+    else:
+      index = 0  # dictionary['UNK']
+      unk_count = unk_count + 1
+    data.append(index)
+  count[0][1] = unk_count
+  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys())) 
+  return data, count, dictionary, reverse_dictionary
+
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+print 'Sample data', data[:10]
+del words  #  reduce memory.
+
+
+data_index = 0
+
+def generate_batch_cbow(batch_size, skip_window):
+  global data_index
+  context_window = 2 * skip_window # calculate the context_window - this is the total number of words around the target
+  assert batch_size % context_window == 0 # ensure the context window can be taken from the batch size
+  num_labels = batch_size / context_window # the number of labels is the how many context windows fit in the batch
+  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+  labels = np.ndarray(shape=(num_labels, 1), dtype=np.int32)
+  span = 2 * skip_window + 1 # [ skip_window target skip_window ]
+  buffer = collections.deque(maxlen=span)
+  for _ in range(span):
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+  for i in range(num_labels):
+    target = skip_window  # target label at the center of the buffer
+    labels[i, 0] = buffer[target] # set he label
+    targets_to_avoid = [ skip_window ]
+    for j in range(context_window):
+      while target in targets_to_avoid:
+        target = random.randint(0, span - 1)
+      targets_to_avoid.append(target)
+      batch[i * context_window + j] = buffer[target]
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+  return batch, labels
+
+skip_window = 1
+batch, labels = generate_batch_cbow(8, skip_window)
+print np.shape(labels)
+for i in range(8):
+  print batch[i], '->', labels[i/(2*skip_window), 0]
+  print reverse_dictionary[batch[i]], '->', reverse_dictionary[labels[i/(2*skip_window), 0]]
+del skip_window # remove skip_window setting used for testing
+
+
+batch_size = 128
+embedding_size = 128 # Dimension of the embedding vector.
+skip_window = 1 # How many words to consider left and right.
+# We pick a random validation set to sample nearest neighbors. here we limit the
+# validation samples to the words that have a low numeric ID, which by
+# construction are also the most frequent. 
+valid_size = 16 # Random set of words to evaluate similarity on.
+valid_window = 100 # Only pick dev samples in the head of the distribution.
+valid_examples = np.array(random.sample(xrange(valid_window), valid_size))
+num_sampled = 32 # Number of negative examples to sample.
+
+## General defines
+context_window = 2 * skip_window
+num_labels = batch_size / context_window
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+  # Input data.
+  train_dataset = tf.placeholder(tf.int32, shape=[batch_size])
+  train_labels = tf.placeholder(tf.int32, shape=[num_labels, 1])
+  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+  
+  # Variables.
+  embeddings = tf.Variable(
+    tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+  softmax_weights = tf.Variable(
+    tf.truncated_normal([vocabulary_size, embedding_size],
+                         stddev=1.0 / math.sqrt(embedding_size)))
+  softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
+  
+  # Model.
+  # Look up embeddings for inputs.
+  embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+
+  # seq_ids only needs to be generated once so do this as a numpy array rather than a tensor.
+  seq_ids = np.zeros(batch_size, dtype=np.int32)
+  cur_id = -1
+  for i in range(batch_size):
+    if i % context_window == 0:
+      cur_id = cur_id + 1
+    seq_ids[i] = cur_id
+  print seq_ids
+  
+  # use segment_sum to add together the related words and reduce the output to be num_labels in size.
+  final_embed = tf.segment_sum(embed, seq_ids)
+  
+  # Compute the softmax loss, using a sample of the negative labels each time.
+  loss = tf.reduce_mean(
+    tf.nn.sampled_softmax_loss(softmax_weights, softmax_biases, final_embed,
+                               train_labels, num_sampled, vocabulary_size))
+
+  # Optimizer.
+  optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
+  
+  # Compute the similarity between minibatch examples and all embeddings.
+  # We use the cosine distance:
+  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+  normalized_embeddings = embeddings / norm
+  valid_embeddings = tf.nn.embedding_lookup(
+    normalized_embeddings, valid_dataset)
+  similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
+  
+  
+  
+  num_steps = 100001
+
+with tf.Session(graph=graph) as session:
+  tf.initialize_all_variables().run()
+  print "Initialized"
+  average_loss = 0
+  for step in xrange(num_steps):
+    batch_data, batch_labels = generate_batch_cbow(batch_size, skip_window)
+    feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
+    _, l = session.run([optimizer, loss], feed_dict=feed_dict)
+    average_loss += l
+    if step % 2000 == 0:
+      if step > 0:
+        average_loss = average_loss / 2000
+      # The average loss is an estimate of the loss over the last 2000 batches.
+      print "Average loss at step", step, ":", average_loss
+      average_loss = 0
+    # note that this is expensive (~20% slowdown if computed every 500 steps)
+    if step % 10000 == 0:
+      sim = similarity.eval()
+      for i in xrange(valid_size):
+        valid_word = reverse_dictionary[valid_examples[i]]
+        top_k = 8 # number of nearest neighbors
+        nearest = (-sim[i, :]).argsort()[1:top_k+1]
+        log = "Nearest to %s:" % valid_word
+        for k in xrange(top_k):
+          close_word = reverse_dictionary[nearest[k]]
+          log = "%s %s," % (log, close_word)
+        print log
+  final_embeddings = normalized_embeddings.eval()
+  
+  
